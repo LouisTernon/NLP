29f5b50cd6cdd080726547b37fe22999bb85fdeb
Louis <louis.ternon@student.ecp.fr>
2020-02-24 14:35:12 +0100

    Modification du code pour que le script de test puisse passer. Le modele s'enregistre a la fin de chaque epoch

--- skipGram.py
+++ skipGram.py
@@ -3,6 +3,7 @@ import argparse
 import pandas as pd
 import time
 import json
+import pickle
 from tqdm import tqdm
 # useful stuff
 import numpy as np
@@ -12,8 +13,8 @@ from sklearn.preprocessing import normalize
 import os
 import re
 
-__authors__ = ['author1', 'author2', 'author3']
-__emails__ = ['fatherchristmoas@northpole.dk', 'toothfairy@blackforest.no', 'easterbunny@greenfield.de']
+__authors__ = ['Louis Ternon', 'Pierre Sevestre', 'Marine Sobas', 'Vincent Ponslet']
+__emails__ = ['louis.ternon@student.ecp.fr', 'pierre.sevestre@student.ecp.fr', 'marine.sobas@student.ecp.fr', 'vincent.ponslet@student.ecp.fr']
 
 
 def raw_to_tokens(raw_string, spacy_nlp):
@@ -24,12 +25,12 @@ def raw_to_tokens(raw_string, spacy_nlp):
     return string_tokens
 
 
-def text2sentences(path, n):
+def text2sentences(path, n=1e12):
     # feel free to make a better tokenization/pre-processing
     """
-	:param path: path to the text files
-	:return: list of all sentences
-	"""
+    :param path: path to the text files
+    :return: list of all sentences
+    """
     # TODO remove non-letter words
 
     sentences = []
@@ -41,21 +42,29 @@ def text2sentences(path, n):
                 break
     return sentences
 
+def similarity(word1, word2, w2id, W):
 
-"""	sentences = []
-	with open(path) as f:
-		for l in f:
-			sentences.append( l.lower().split() )
-	return sentences
-"""
+    """
+    computes similiarity between the two words. unknown words are mapped to one common vector
+    :param word1:
+    :param word2:
+    :return: a float \in [0,1] indicating the similarity (the higher the more similar)
+    """
+    try:
+        a, b = int(w2id[word1]), int(w2id[word2])
+        e1, e2 = W[a, :], W[b, :]
+        return e1.T @ e2 / (np.linalg.norm(e1) * np.linalg.norm(e2))
+    except:
+        return 0
+
 
 
 def loadPairs(path):
-    """
 
-	:param path: path to the csv of word pairs
-	:return: zip object of word pairs
-	"""
+    """
+    :param path: path to the csv of word pairs
+    :return: zip object of word pairs
+    """
 
     data = pd.read_csv(path, delimiter='\t')
     pairs = zip(data['word1'], data['word2'], data['similarity'])
@@ -64,10 +73,11 @@ def loadPairs(path):
 
 class SkipGram():
 
-    def __init__(self, sentences=1, nEmbed=30, negativeRate=0.0001, winSize=5, winNegSize=5, minFreq=0, maxFreq=1,
-                 batchSize=256, monitor_results=False, experience_tag=""):
+    def __init__(self, sentences=1, nEmbed=30, negativeRate=0.0001, winSize=5, winNegSize=5, minCount=0, maxFreq=1,
+                  monitor_results=False, experience_tag=""):
 
-        self.valid_vocab, self.vocab_occ, self.eliminated_words = self.initialize_vocab(sentences, minFreq, maxFreq)
+        self.valid_vocab, self.vocab_occ, self.eliminated_words = self.initialize_vocab(sentences, minCount, maxFreq)
+
         # set of valid words  over the whole dataset, dict of their occurences, and set of all eliminated words
         print(f"Num words : {len(self.valid_vocab)}; eliminated : {len(self.eliminated_words)}")
         self.w2id, self.id2w = self.word_id()  # word to ID mapping
@@ -95,13 +105,22 @@ class SkipGram():
             else:
                 print("{} already exists, might overwrite".format(self.output_path))
 
-    def initialize_vocab(self, sentences, minFreq, maxFreq):
+    def initialize_vocab(self, sentences, minCount, maxFreq):
+        """
+        Eliminates words that appear less than minCount times from the dataset
+        Eliminates words that have a frequency greater than maxFreq. We chose this specificaly to remove over represented
+        words like "the" or "he"
+        :param sentences: list[list[str]]. List of all sentences
+        :param minCount:
+        :param maxFreq: in the initialisation of the class, it is set by default to 1 so no word is elimminated
+        :return:
+        """
         all_words = [word for sentence in sentences for word in sentence]  # list of all the words of all sentences
         all_words_unique = set(all_words)
         vocab_occ = {word: 0 for word in all_words_unique}
         for word in all_words:
             vocab_occ[word] += 1
-        minCount,  maxCount = len(all_words)*minFreq, len(all_words)*maxFreq
+        maxCount = len(all_words)*maxFreq
         valid_words_set1 = set(filter(lambda x: vocab_occ[x] > minCount, all_words_unique))
         valid_words_set2 = set(filter(lambda x: vocab_occ[x] < maxCount, all_words_unique))
         valid_words_set = valid_words_set1.intersection(valid_words_set2)
@@ -111,9 +130,9 @@ class SkipGram():
     def word_id(self):
 
         """
-		:param setntences: set of all words in the vocab, occ > minCount
-		:return: dictionaries {word:id} and {id:word}
-		"""
+        :param setntences: set of all words in the vocab, occ > minCount
+        :return: dictionaries {word:id} and {id:word}
+        """
 
         return {word: q for q, word in enumerate(self.valid_vocab)}, \
                {q: word for q, word in enumerate(self.valid_vocab)}
@@ -121,10 +140,10 @@ class SkipGram():
     def filter_sentences(self, invalid_vocab, sentences):
         """
 
-		:param invalid_vocab: all words that must be extracted from the dataset
-		:param sentences: list of sentences that constitute the dataset
-		:return: sentences where all the invalid vocab has been deleted
-		"""
+        :param invalid_vocab: all words that must be extracted from the dataset
+        :param sentences: list of sentences that constitute the dataset
+        :return: sentences where all the invalid vocab has been deleted
+        """
         for q, sentence in enumerate(sentences):
             to_del = []
             for p, word in enumerate(sentence):
@@ -138,12 +157,12 @@ class SkipGram():
     def buildTrainSet(self, sentences, winSize, dynamic=False, shuffle=True):
         """
 
-		:param sentences: list of filtered sentences
-		:param winSize: context size
-		:param dynamic: if True, builds dynamic context windows
-		:param suffle: if True, all
-		:return: 2-D array. Each row is center_word, context_word. If the context has k words, then there are k rows created
-		"""
+        :param sentences: list of filtered sentences
+        :param winSize: context size
+        :param dynamic: if True, builds dynamic context windows
+        :param suffle: if True, all
+        :return: 2-D array. Each row is center_word, context_word. If the context has k words, then there are k rows created
+        """
         trainset = []
         for sentence in sentences:
             for k, word in enumerate(sentence):
@@ -165,11 +184,12 @@ class SkipGram():
     def build_neg_vocab(self, vocab, vocab_occ, neg_treshold):
         """
 
-		:param vocab: all the word that will be in the train set and occur at least minCount times
-		:param vocab_occ: dict where {valid word  : number of occurences}
-		:param neg_treshold: maximum time a word can occur to be qualified for the negative words set
-		:return:
-		"""
+        :param vocab: all the word that will be in the train set and occur at least minCount times
+        :param vocab_occ: dict where {valid word  : number of occurences}
+        :param neg_treshold: maximum time a word can occur to be qualified for the negative words set
+        :return neg_vocag_set: set of all the words that can be negatively sampled
+        :return n_neg: len(neg_vocab)
+        """
 
         n = np.sum(vocab_occ[word] for word in vocab)
         vocab_freq = {word: vocab_occ[word] / n for word in vocab}
@@ -182,10 +202,10 @@ class SkipGram():
     def build_neg_distrib(self, neg_vocab, distrib=None):
 
         """
-		:param neg_vocab: words that are eligible to be negatively sampled
-		:param distrib: type of distribution to the negative sampling
-		:return: a dictionary with the sampling frequency of each word
-		"""
+        :param neg_vocab: words that are eligible to be negatively sampled
+        :param distrib: type of distribution to the negative sampling
+        :return: a dictionary with the sampling frequency of each word
+        """
 
         if distrib == "basic":
             return self.build_neg_distrib_basic(neg_vocab)
@@ -196,9 +216,9 @@ class SkipGram():
     def build_neg_distrib_basic(self, neg_vocab):
 
         """
-		:param neg_vocab: list of all the words that qualify for the negative vocabulary
-		:return: a dictionary with the frequency of each word of the negative vocabulary
-		"""
+        :param neg_vocab: list of all the words that qualify for the negative vocabulary
+        :return: a dictionary with the frequency of each word of the negative vocabulary
+        """
         n = np.sum(self.vocab_occ[word] for word in neg_vocab)
         distr = {neg_w: self.vocab_occ[neg_w] / n for neg_w in neg_vocab}
 
@@ -207,10 +227,10 @@ class SkipGram():
     def transform_34(self, neg_vocab):
 
         """
-		:param neg_vocab: words that are eligible to be negatively sampled
-		:return normed_distr: a dictionary with the frequency of each word of the negative vocabulary. Each word receives a ^0.75
-		transformation. Then all frequencies are normalised.
-		"""
+        :param neg_vocab: words that are eligible to be negatively sampled
+        :return normed_distr: a dictionary with the frequency of each word of the negative vocabulary. Each word receives a ^0.75
+        transformation. Then all frequencies are normalised.
+        """
 
         n = np.sum(self.vocab_occ[word] for word in neg_vocab)
         distr = {neg_w: (self.vocab_occ[neg_w] / n) ** 0.75 for neg_w in neg_vocab}
@@ -221,13 +241,13 @@ class SkipGram():
 
     def parse_sentence_for_context(self, sentence, K):
         """
-		the windSize is defined at the initialisation of the class
+        the windSize is defined at the initialisation of the class
 
-		:param K: int, size of the context window
-		:param sentence: str,
-		:return: list of center words list(str), and a list with their corresponding context (list(list(str))
+        :param K: int, size of the context window
+        :param sentence: str,
+        :return: list of center words list(str), and a list with their corresponding context (list(list(str))
 
-		"""
+        """
         center_words = []
         contexts = []
         for k, word in enumerate(sentence):
@@ -241,17 +261,12 @@ class SkipGram():
     def select_negative_sampling(self, neg_distribution, K):
         """
 
-		:param neg_vocab: set of the negative vocab
-		:param neg_distribution: {neg_word : sample rate}
-		:return: returns K negative words
-		"""
+        :param neg_vocab: set of the negative vocab
+        :param neg_distribution: {neg_word : sample rate}
+        :return: returns K negative words
+        """
 
         return [np.random.choice(list(neg_distribution.keys()), p=list(neg_distribution.values())) for x in range(K)]
-
-    def oneHot(self, dim, id):
-        oh = np.zeros(dim, int)
-        oh[id] = 1
-        return oh
 
     def clippedSigm(self, x):
         return expit(np.clip(x, -6, 6))  # Clip values at 6 to avoid extreme values at the gradient
@@ -259,13 +274,12 @@ class SkipGram():
     def forward(self, centerBatch, contextBatch, negativeBatch, negWinSize):
 
         """
-
-		:param self:
-		:param center_word: word of the center context to evaluate
-		:param target_context_word: one word of the context
-		:param neg_words: list of negative words
-		:return: loss function and intermediate gradients
-		"""
+        :param self:
+        :param center_word: word of the center context to evaluate
+        :param target_context_word: one word of the context
+        :param neg_words: list of negative words
+        :return: loss function and intermediate gradients
+        """
 
         gradW = np.zeros(self.W.shape)
         gradWp = np.zeros(self.Wp.shape)
@@ -314,22 +328,22 @@ class SkipGram():
         self.W -= lr * gradW
         self.Wp -= lr * gradsWp
 
-    def train(self, epochs, lr, batchSize):
+    def train(self, epochs, lr, batchSize, path):
 
         """
 
-		:param trainset: list of all (center word, context word). One context word at a time. Can be shuffled
-		:param batchSize:
-		:param lr: learning rate
-		:param epochs:
-		:param negWinSize: size of the negative sampling. 2-5 for a large dataset. 5-20 for a small dataset
-		:return:
-		"""
+        :param trainset: list of all (center word, context word). One context word at a time. Can be shuffled
+        :param batchSize:
+        :param lr: learning rate
+        :param path: path to save the model's weights after each epoch
+        :return:
+        """
 
         start_time = time.time()
         self.loss_history = []
         self.posloss_history = []
         self.negloss_history = []
+
 
         for epoch in range(epochs):
 
@@ -363,7 +377,7 @@ class SkipGram():
 
                 if self.monitor_results and q % 1000 == 0:
                     pass
-                    # print("Similarity of Monday - Tuesday : {} / Monday - Financial : {}".format(self.similarity("monday", "tuesday"), self.similarity("monday", "financial")))
+                    #print("Similarity of Monday - Tuesday : {} / Monday - Financial : {}".format(self.similarity("monday", "tuesday"), self.similarity("monday", "financial")))
 
             if self.monitor_results:
                 print("Epoch {} | Word Count :{} | Loss : {:.3f} | Neg/Pos : {:.3f}/{:.3f} | Eta : {:.3f}s".format(epoch+1, counter, accloss / counter, loss_pos / counter, loss_neg / counter, (epochs-(epoch+1))*((time.time()-start_time)/(epoch+1))))
@@ -375,45 +389,45 @@ class SkipGram():
                 np.save(os.path.join(self.output_path, "posloss.npy"), np.array(self.posloss_history))
                 np.save(os.path.join(self.output_path, "negloss.npy"), np.array(self.negloss_history))
 
+            self.save(path)
+
     def save(self, path):
         """
-		:param self:
-		:param path: path to which the model is to be saved
-		:return: None, saves the model's weighs to path
-		"""
+        :param self:
+        :param path: path to which the model is to be saved
+        :return: None, saves the model's weighs to path
+        """
 
-        with open('w2id.json', 'w') as fp:
-            json.dump(self.w2id, fp)
+        keys = np.array(list(self.w2id.keys()))
+        ids = np.array(list(self.w2id.values()))
+        model = np.vstack((keys, ids, self.W.T)).T
 
-        # Save
-        np.save(path + 'W.npy', self.W)
-        np.save(path + 'Wp.npy', self.Wp)
+        with open(path, 'wb') as fp:
+            pickle.dump(model, fp)
 
+    @staticmethod
+    def load(path):
+        with open(path, 'rb') as file:
+            model = pickle.load(file)
+        return model
+    """
     def load(self, path):
-        """
+        
 
-		:param self:
-		:param path: path to the model
-		:return: None, initiates the model with all the weighs saved previously
-		"""
+        :param self:
+        :param path: path to the model
+        :return: None, initiates the model with all the weighs saved previously
+        
 
-        self.W = np.load(path + 'W.npy')
-        self.Wp = np.load(path + 'Wp.npy')
+        
+        with open(path, 'rb') as fp:
+            model = pickle.load(fp)
+            self.w2id = {model[0,k] : model[1,k] for k in range(model.shape[0])}
+            self.W = model[:, 2:]
+            
+    """
 
-        with open('w2id.json', 'r') as fp:
-            self.w2id = json.load(fp)
 
-    def similarity(self, word1, word2):
-        """
-		computes similiarity between the two words. unknown words are mapped to one common vector
-		:param word1:
-		:param word2:
-		:return: a float \in [0,1] indicating the similarity (the higher the more similar)
-		"""
-        a, b = self.w2id[word1], self.w2id[word2]
-        e1, e2 = self.W[a, :], self.W[b, :]
-
-        return e1.T @ e2 / (np.linalg.norm(e1) * np.linalg.norm(e2))
 
 if __name__ == '__main__':
 
@@ -426,14 +440,29 @@ if __name__ == '__main__':
 
     if not opts.test:
         sentences = text2sentences(opts.text)
-        sg = SkipGram(sentences)
-        sg.train()
-        sg.save(opts.model)
+        n = len(sentences)
+
+        if len(sentences) <= 1000:
+            sg = SkipGram(sentences, nEmbed = 50, negativeRate = 0.01, winSize = 5, winNegSize = 5, minCount = 0, maxFreq = 1,
+            monitor_results = False, experience_tag = "")
+            sg.train(epochs = 100, batchSize = 16, lr = 0.05, path = opts.model)
+        if 1000 < len(sentences) and len(sentences) <= 100000:
+            sg = SkipGram(sentences, nEmbed = 150, negativeRate = 0.001, winSize = 5, winNegSize = 5, minCount = 5, maxFreq = 1,
+            monitor_results = False, experience_tag = "")
+            sg.train(epochs = 150, batchSize = 128, lr = 0.05, path = opts.model)
+        if len(sentences) > 100000:
+            sg = SkipGram(sentences, nEmbed = 300, negativeRate = 0.0001, winSize = 5, winNegSize = 2, minCount = 20, maxFreq = 1,
+            monitor_results = False, experience_tag = "")
+            sg.train(epochs = 200, batchSize = 256, lr = 0.01, path = opts.model)
+
 
     else:
         pairs = loadPairs(opts.text)
 
-        sg = SkipGram.load(opts.model)
+        model = SkipGram.load(opts.model)
+        w2id = {model[k, 0]:model[k, 1] for k in range(model.shape[0])}
+        W = model[:, 2:].astype(np.float)
+
         for a, b, _ in pairs:
             # make sure this does not raise any exception, even if a or b are not in sg.vocab
-            print(sg.similarity(a, b))
+            print(similarity(a, b, w2id, W))
